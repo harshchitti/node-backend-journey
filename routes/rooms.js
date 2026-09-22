7e6a697c139d8d596dfcab151efbd1e3644bcd24
chitti harsh <chittiharsh3@gmail.com>
2026-09-22 09:15:42 +0530

    added express-validator and changed post/rooms according;y using express validator

--- routes/rooms.js
+++ routes/rooms.js
@@ -3,25 +3,29 @@ import 'dotenv/config';
 import pool from '../db.js';
 import  verifyToken  from '../middleware/verifyToken.js';
 import requireAdmin from '../middleware/requireAdmin.js';
+import {body,validationResult} from 'express-validator';
 const router = express.Router();
-router.post('/',verifyToken,requireAdmin,async (req,res)=>{
-    try{
-    const{name} = req.body;
-    if(name === undefined || name.trim() === ""){
-        return res.status(400).send("not a valid input");
+router.post('/',
+    verifyToken,
+    requireAdmin,
+    body('name').trim().notEmpty().withMessage('name is required'),
+    async(req,res)=>{
+        const errors=validationResult(req);
+        if(!errors.isEmpty())return res.status(400).json({ errors: errors.array() });
+        try{
+          const{name} = req.body;
+          const result=await pool.query(
+          'insert into rooms (name) values ($1) returning *',
+          [name]
+         );
+         return res.status(201).json(result.rows[0]);
+        }catch(err){
+         console.log(err);
+         if(err.code==="23505")return res.status(409).send("room number already exists");
+         return res.status(500).send("something went wrong");
+       } 
     }
-    const result=await pool.query(
-        'insert into rooms (name) values ($1) returning *',
-        [name]
-    );
-    return res.status(201).json(result.rows[0]);
-    }catch(err){
-       console.log(err);
-       if(err.code==="23505")return res.status(409).send("room number already exists");
-       return res.status(500).send("something went wrong");
-    }
-
-});
+);
 router.get('/',verifyToken,async (req,res)=>{
       try{
            const result = await pool.query(
@@ -44,7 +48,7 @@ router.patch('/:id',verifyToken,requireAdmin, async(req,res)=>{
         [status,id]
     );
     if(result.rows.length===0)return res.status(404).send("no room found");
-    return res.status(200).json(result.rows.[0]);
+    return res.status(200).json(result.rows[0]);
    }catch(err){
         console.log(err);
         return res.status(500).send("something went wrong");
